bf55fb1332b461f81c3b11f93b08d100aacf14ea
theangelskies <theangelskies13@yahoo.com>
2026-04-02 00:41:29 +0200

    Add jq command to output the name from person.json

--- jq/script-01.sh
+++ jq/script-01.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
-
+jq -r '.name' person.json
 # The input for this script is the person.json file.
 # TODO: Write a command to output the name of the person.
 # Your output should be exactly the string "Selma", but should not contain any quote characters.
