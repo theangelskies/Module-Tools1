2c4a9ab155a52c8fa4ea04ea71f55bb34c090f31
theangelskies <theangelskies13@yahoo.com>
2026-04-02 00:56:50 +0200

    Add jq command to output the total of all players' first scores from scores.json

--- jq/script-10.sh
+++ jq/script-10.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
-
+jq -r '[.[].scores[0]] | add' scores.json
 # The input for this script is the scores.json file.
 # TODO: Write a command to output the total of adding together all players' first scores.
 # Your output should be exactly the number 54.
