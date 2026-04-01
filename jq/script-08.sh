580b5cce9ebc662fd9ef45f641eddbbde86cf6c4
theangelskies <theangelskies13@yahoo.com>
2026-04-02 00:53:54 +0200

    Add jq command to output player names and their scores count from scores.json

--- jq/script-08.sh
+++ jq/script-08.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
-
+jq -r '.[] | "\(.name) \(.scores | length)"' scores.json    
 # The input for this script is the scores.json file.
 # TODO: Write a command to output just the names of each player along with the number of times they've played the game.
 # Your output should contain 6 lines, each with one word and one number on it.
