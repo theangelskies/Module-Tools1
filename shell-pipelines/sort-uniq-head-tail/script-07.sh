b923b67efc6d747b06ecd1e473d4b1115c2c79bb
theangelskies <theangelskies13@yahoo.com>
2026-04-02 18:20:52 +0200

    command to show how many times anyone has entered and exited

--- shell-pipelines/sort-uniq-head-tail/script-07.sh
+++ shell-pipelines/sort-uniq-head-tail/script-07.sh
@@ -6,3 +6,5 @@ set -euo pipefail
 # TODO: Write a command to show how many times anyone has entered and exited.
 # It should be clear from your script's output that there have been 5 Entry events and 4 Exit events.
 # The word "Event" should not appear in your script's output.
+echo "Entry $(grep -c 'Entry' events-with-timestamps.txt)"
+echo "Exit $(grep -c 'Exit' events-with-timestamps.txt)"
