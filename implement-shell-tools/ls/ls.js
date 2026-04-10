b888a31d757b2c9c1920a1f779ca988b6e206a78
theangelskies <theangelskies13@yahoo.com>
2026-04-10 11:49:11 +0200

    Add initial implementation of ls tool to list directory contents

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -1,1 +1,13 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
+
+const args = process.argv.slice(2);
+
+const dir = args[0] || ".";
+
+const entries = fs.readdirSync(dir);
+
+for (const entry of entries) {
+  console.log(entry);
+}
