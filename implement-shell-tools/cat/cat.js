d0e3ebebd0813aba392b6720baf02b9401786628
theangelskies <theangelskies13@yahoo.com>
2026-04-10 10:51:52 +0200

    Implement line numbering option for cat tool

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -2,9 +2,30 @@
 
 const fs = require("fs");
 
-const files = process.argv.slice(2);
+let args = process.argv.slice(2);
+
+let numberLines = false;
+
+if (args[0] === "-n") {
+  numberLines = true;
+  args.shift();
+}
+
+const files = args;
+
+let lineNumber = 1;
 
 for (const file of files) {
   const content = fs.readFileSync(file, "utf8");
-  process.stdout.write(content);
+
+  if (!numberLines) {
+    process.stdout.write(content);
+  } else {
+    const lines = content.split("\n");
+
+    for (const line of lines) {
+      process.stdout.write(`${String(lineNumber).padStart(6)}  ${line}\n`);
+      lineNumber++;
+    }
+  }
 }
