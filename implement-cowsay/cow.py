612dbecba336bec78bf20a6883fa2284072f30bb
theangelskies <theangelskies13@yahoo.com>
2026-04-16 17:11:34 +0200

    Get available animals dynamically from cowsay

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -3,5 +3,12 @@ import cowsay
 
 
 def main():
+    animals = cowsay.char_names
+
+    parser = argparse.ArgumentParser(
+        prog="cowsay",
+        description="Make animals say things"
+    )
+
  
     main()
