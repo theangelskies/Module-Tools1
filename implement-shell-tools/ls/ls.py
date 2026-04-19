9d34f12dfe261fee8df5a4e73f8fc1521c337701
theangelskies <theangelskies13@yahoo.com>
2026-04-19 14:36:19 +0200

    ls command to accept directory path as an argument

--- implement-shell-tools/ls/ls.py
+++ implement-shell-tools/ls/ls.py
@@ -1,6 +1,15 @@
 import os
+import sys
 
-files = os.listdir(".")
+args = sys.argv[1:]
+
+path = "."
+
+for arg in args:
+    if not arg.startswith("-"):
+        path = arg
+
+files = os.listdir(path)
 
 for f in files:
     print(f)
