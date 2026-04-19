bad70a25d33e5fd50e62dff2f318cd2d2e4cd7a9
theangelskies <theangelskies13@yahoo.com>
2026-04-19 14:17:47 +0200

    Enhance cat command to support -b option for numbering non-blank lines

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -3,20 +3,33 @@ import sys
 args = sys.argv[1:]
 
 number = False
+number_nonblank = False
 files = []
 
 for arg in args:
     if arg == "-n":
         number = True
+    elif arg == "-b":
+        number_nonblank = True
     else:
         files.append(arg)
+
+# -b overrides -n
+if number_nonblank:
+    number = False
 
 line_number = 1
 
 for file in files:
     with open(file, "r") as f:
         for line in f:
-            if number:
+            if number_nonblank:
+                if line.strip():
+                    print(f"{line_number}\t{line}", end="")
+                    line_number += 1
+                else:
+                    print(line, end="")
+            elif number:
                 print(f"{line_number}\t{line}", end="")
                 line_number += 1
             else:
