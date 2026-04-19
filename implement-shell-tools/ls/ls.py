716159158dcf544fc632bb1043eb8c1e5745b03a
theangelskies <theangelskies13@yahoo.com>
2026-04-19 14:38:28 +0200

    show hidden files

--- implement-shell-tools/ls/ls.py
+++ implement-shell-tools/ls/ls.py
@@ -3,13 +3,18 @@ import sys
 
 args = sys.argv[1:]
 
+show_all = False
 path = "."
 
 for arg in args:
-    if not arg.startswith("-"):
+    if arg == "-a":
+        show_all = True
+    elif not arg.startswith("-"):
         path = arg
 
 files = os.listdir(path)
 
 for f in files:
+    if not show_all and f.startswith("."):
+        continue
     print(f)
