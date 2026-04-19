b9375e7c8b9338e9f91bf2a17a82cc351a5441da
theangelskies <theangelskies13@yahoo.com>
2026-04-19 14:44:24 +0200

    Refactor ls command implementation for improved structure and error handling; add wc.py file

--- implement-shell-tools/ls/ls.py
+++ implement-shell-tools/ls/ls.py
@@ -1,20 +1,33 @@
 import os
 import sys
 
-args = sys.argv[1:]
+def ls(path=".", show_all=False):
+    try:
+        files = os.listdir(path)
 
-show_all = False
-path = "."
+        for f in files:
+            if not show_all and f.startswith("."):
+                continue
+            print(f)
 
-for arg in args:
-    if arg == "-a":
-        show_all = True
-    elif not arg.startswith("-"):
-        path = arg
+    except FileNotFoundError:
+        print(f"ls: cannot access '{path}': No such file or directory")
 
-files = os.listdir(path)
 
-for f in files:
-    if not show_all and f.startswith("."):
-        continue
-    print(f)
+def main():
+    args = sys.argv[1:]
+
+    show_all = False
+    path = "."
+
+    for arg in args:
+        if arg == "-a":
+            show_all = True
+        elif not arg.startswith("-"):
+            path = arg
+
+    ls(path, show_all)
+
+
+if __name__ == "__main__":
+    main()
